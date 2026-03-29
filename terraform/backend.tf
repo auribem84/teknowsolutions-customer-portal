6ba18a4c7c72e52d76934bcd1e8b754e69144542
Arturo Uribe <auribe21@Mac.lan>
2026-03-28 22:59:50 -0400

    refactor terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "auribem84_org" # Reemplaza con tu organización real en TF Cloud
 
     workspaces {
-      name = "car-app"  # Reemplaza con el nombre de tu workspace
+      name = "app-car"  # Reemplaza con el nombre de tu workspace
     }
   }
 }
