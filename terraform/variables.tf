3671991eaaef43fec8497809c89102f69a9f07b8
Arturo Uribe <auribe21@Mac.lan>
2026-03-28 21:49:57 -0400

    modify secrets

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,13 @@
 # No necesitas asignarles un 'default' aquí, 
 # Terraform Cloud las llenará por ti al ejecutar el plan.
 
-variable "RENDER_API_KEY" {
+variable "render_api_key" {
   type      = string
   sensitive = true
   default   = "rnd_5pytohBuTRMIbxyKpTo9zcXK7M2R"
 }
 
-variable "RENDER_OWNER_ID" {
+variable "render_owner_id" {
   type      = string
   default   = "usr-d6tjt6q4d50c73cc912g"
 }
