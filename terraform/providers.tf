cb54ccd745b950950071045111ee1457600084d2
Arturo Uribe <auribe21@Mac.lan>
2026-03-28 22:43:36 -0400

    update providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = ">= 1.3.0"
+  # CONFIGURACIÓN DE TERRAFORM CLOUD
+  cloud {
+    organization = "auribem84_org" # <--- Cambia esto
+
+    workspaces {
+      name = "car-app" # <--- Cambia esto
+    }
+  }
 
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = "~> 1.8.0"
+      version = "~> 1.0"
     }
   }
 }
